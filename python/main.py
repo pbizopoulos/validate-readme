606da8106d59469e21e14fd36b72c327c124b167
Paschalis Bizopoulos <pbizop@gmail.com>
2023-11-22 22:12:30 +0200

    .

--- python/main.py
+++ python/main.py
@@ -1,14 +1,44 @@
+from __future__ import annotations
+
+import sys
 import unittest
+from io import TextIOWrapper
 from pathlib import Path
 
-from python.main import validate_readme
+from lark import Lark
+
+grammar = """
+start: title requirements? download? development? staging? production? installation? usage?
+title: "# " LINE+ "\\n"
+requirements: "# Requirements" "\\n" LINE+ "\\n"
+download: "# Download\\n" LINE+ "\\n"
+development: "# Development\\n" LINE+ "\\n"
+staging: "# Staging\\n" LINE+ "\\n"
+production: "# Production\\n" LINE+ "\\n"
+installation: "# Installation\\n" LINE+ "\\n"
+usage: "# Usage\\n" LINE+ "\\n"
+LINE: /.+[^\\s]\\n/
+"""  # noqa: E501
+
+
+def validate_readme(code_input: str | TextIOWrapper) -> None:
+    parser = Lark(grammar, parser="lalr")
+    if isinstance(code_input, str):
+        with Path(code_input).open() as file:
+            parser.parse(file.read())
+    elif isinstance(code_input, TextIOWrapper):
+        parser.parse(code_input.read())
 
 
 class Tests(unittest.TestCase):
-    def test_validate_readme(self: "Tests") -> None:
+    def test_validate_readme(self: Tests) -> None:
         with Path("prm/README").open(encoding="utf-8") as file:
             validate_readme(file)
 
 
 if __name__ == "__main__":
     unittest.main()
+else:
+    num_arguments_allowed = 2
+    if len(sys.argv) == num_arguments_allowed:
+        validate_readme(sys.argv[1])
